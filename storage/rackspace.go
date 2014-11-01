e2511f0b775e969c317b03be71893e7198df0d21
Paul Querna <pquerna@apache.org>
2014-11-01 08:01:13 -0700

    Add listing of files

--- storage/rackspace.go
+++ storage/rackspace.go
@@ -20,9 +20,21 @@ package storage
 import (
 	"github.com/pquerna/distsync/common"
 	"github.com/pquerna/distsync/crypto"
+	"github.com/rackspace/gophercloud"
+	osObjects "github.com/rackspace/gophercloud/openstack/objectstorage/v1/objects"
+	"github.com/rackspace/gophercloud/pagination"
+	"github.com/rackspace/gophercloud/rackspace"
+	"github.com/rackspace/gophercloud/rackspace/objectstorage/v1/objects"
 
 	"errors"
 	"io"
+	"time"
+)
+
+const (
+	// This is basically time.RFC3339Nano, but missing the timezone.
+	// Swift docs say its always in UTC though. So. Here. We. Go.
+	swiftTimelayout = "2006-01-02T15:04:05.999999999"
 )
 
 type CloudFilesStorage struct {
@@ -45,12 +57,67 @@ func NewCloudFiles(creds *common.RackspaceCreds, bucket string) (*CloudFilesStor
 	}, nil
 }
 
+func (cf *CloudFilesStorage) client() (*gophercloud.ServiceClient, error) {
+	auth := gophercloud.AuthOptions{
+		Username: cf.creds.Username,
+		APIKey:   cf.creds.ApiKey,
+	}
+
+	ac, err := rackspace.AuthenticatedClient(auth)
+	if err != nil {
+		return nil, err
+	}
+
+	// TOOD: auto-detect serviceNet?
+	return rackspace.NewObjectStorageV1(ac, gophercloud.EndpointOpts{
+		Region: cf.creds.Region,
+	})
+}
+
 func (cf *CloudFilesStorage) Download(filename string, writer io.Writer) error {
 	return errors.New("not done")
 }
 
 func (cf *CloudFilesStorage) List(dc crypto.Decryptor) ([]*FileInfo, error) {
-	return nil, errors.New("not done")
+	client, err := cf.client()
+	if err != nil {
+		return nil, err
+	}
+
+	rv := make([]*FileInfo, 0)
+	err = objects.List(client, cf.bucket, osObjects.ListOpts{Full: true}).EachPage(func(p pagination.Page) (bool, error) {
+		objs, err := objects.ExtractInfo(p)
+		if err != nil {
+			return false, err
+		}
+		for _, obj := range objs {
+			if obj.Name == ".distsync" {
+				continue
+			}
+
+			lm, err := time.Parse(swiftTimelayout, obj.LastModified)
+			if err != nil {
+				return false, err
+			}
+			name := ""
+			if dc != nil {
+				name, err = dc.DecryptName(obj.Name)
+				if err != nil {
+					return false, err
+				}
+			}
+
+			rv = append(rv, &FileInfo{
+				EncryptedName: obj.Name,
+				Name:          name,
+				LastModified:  lm,
+				Length:        int64(obj.Bytes),
+			})
+		}
+		return true, nil
+	})
+
+	return rv, nil
 }
 
 func (cf *CloudFilesStorage) Upload(filename string, reader io.ReadSeeker) error {
